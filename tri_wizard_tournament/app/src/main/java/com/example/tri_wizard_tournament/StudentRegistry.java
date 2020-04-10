c82b8713e9d1806c64408f6aebb1cd349743772c
Neo <ndivhuwo.nthambeleni@standardbank.co.za>
2020-04-10 21:21:37 +0200

    current changes. Creation of the studentRegistry page alongside the layout, also with the studentRegistryAdapter.

---
+++ tri_wizard_tournament/app/src/main/java/com/example/tri_wizard_tournament/StudentRegistry.java
@@ -0,0 +1,13 @@
+package com.example.tri_wizard_tournament;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudentRegistry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_registry);
+    }
+}
---
+++ tri_wizard_tournament/app/src/main/java/com/example/tri_wizard_tournament/StudentRegistryAdapter.java
@@ -0,0 +1,13 @@
+package com.example.tri_wizard_tournament;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudentRegistryAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_registry_adapter2);
+    }
+}
